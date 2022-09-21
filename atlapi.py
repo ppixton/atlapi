da65be583b0b21a8a1ac910a31a686344831acb1
Parker Pixton <parker.pixton@gmail.com>
2022-09-20 21:40:50 -0600

    Added affilition and proper headings

--- atlapi.py
+++ atlapi.py
@@ -11,16 +11,42 @@ for i in range(26):
     response = requests.get(url)
 
     for x in response.json():
-
         if ("(" not in x['name']):
             names.append(x['name'])
+        
+        if ("affiliation" in x): 
+            if ("Air" in x['affiliation']):
+                affiliation.append('Air')
+            elif ("Fire" in x['affiliation']):
+                affiliation.append('Fire')
+            elif ("Water" in x['affiliation']):
+                affiliation.append('Water')
+            elif ("Earth" in x['affiliation']):
+                affiliation.append('Earth')
+            else:
+                affiliation.append('None')
+        else:
+            affiliation.append("")
+        
 
-header = ['first name', 'last name']
+header = ['Employee Id', 'First Name', 'Last Name', 'Preferred First Name', 'Email', 'Secondary Email', 'Phone Number', 
+'Username', 'Password', 'Hire Date', 'Birth Date', 'Role', 'Country Code', 'Supervisor Id', 'Group Name', 'Langauge', "#Affiliation"]
 data = []
+
+i = 0
 
 for x in names:
     first_name = ""
     last_name = ""
+    id = ""
+    i +=1
+    if (i >=10 and i<100):
+        id = "0" + str(i)
+    elif (i >=100):
+        id = str(i)
+    else:
+        id = "00" + str(i)
+
     spaces = x.count(" ")
     if (spaces >= 1):
         holder = x.index(" ")
@@ -28,9 +54,11 @@ for x in names:
         last_name = x[holder+1:len(x)]
     else:
         first_name = x
-    data.append([first_name, last_name])
+    data.append([id, first_name, last_name, '', '', '', '', '', '', '', '', '', '', '', '', '', affiliation[i]])
 
-file_name= ""
+
+
+file_name= "fournations.csv"
 with open(file_name, 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
 
@@ -41,5 +69,5 @@ with open(file_name, 'w', encoding='UTF8', newline='') as f:
     writer.writerows(data)
 
 
-print(data)
+# print(data)
 
